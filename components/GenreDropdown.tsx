7c5a5855f6889c937bf6339aec79d7a5d32ff40b
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2024-01-15 14:22:04 +0530

    built: GET Data from TMDB Api

--- components/GenreDropdown.tsx
+++ components/GenreDropdown.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+require("dotenv").config();
 
-function GenreDropdown() {
+async function GenreDropdown() {
+  const url =
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+    next: {
+      revalidate: 60 * 60 * 24, //24 hours
+    },
+  };
+
+  const response = await fetch(url, options);
+
   return <div>GenreDropdown</div>;
 }
 
