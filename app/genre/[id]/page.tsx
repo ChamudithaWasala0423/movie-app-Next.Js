be8c76575e624e73d7e5eebaca25658c2955ace9
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2024-01-16 13:50:26 +0530

    change styles

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { notFound } from "next/navigation";
+import { getDiscoverMovies } from "@/lib/getMovies";
+import MoviesCarousel from "@/components/MovieCarousel";
 
 type Props = {
   params: {
@@ -10,10 +12,15 @@ type Props = {
   };
 };
 
-function GenerePage({ params: { id }, searchParams: { genre } }: Props) {
+async function GenerePage({ params: { id }, searchParams: { genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
   return (
-    <div className="bg-red-500">
-      Welcome to genre ID: {id} and {genre}
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        {/* Azure OPENAI service Sugesions */}
+        <h1 className="text-6xl font-bold px-0">Results for {genre}</h1>
+        <MoviesCarousel movies={movies} title={genre} isVertical />
+      </div>
     </div>
   );
 }
